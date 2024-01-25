825967c24d992aa0df7f3934267cff8934ba27a6
albert <albertprofe@gmail.com>
2024-01-25 18:48:45 +0100

    add create order component wiht form, useState, button

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,26 @@
 import MenuList from "./MenuList.jsx";
-import Order from "./Order.jsx";
+import CreateOrder from "./CreateOrder.jsx";
+import "w3-css/w3.css";
 
 export default function App() {
   return (
     <>
       <div class="w3-container">
         <h1>healthy food</h1>
-        <div class="w3-container w3-cell">
-          <MenuList />
-        </div>
-        <div class="w3-container w3-cell">
-          <MenuList />
-        </div>
-        <div class="w3-container w3-cell">
-          <MenuList />
-        </div>
-        <MenuList />
       </div>
+      <div class="w3-row">
+        <div class="w3-col m4">
+          <MenuList />
+        </div>
+        <div class="w3-col m4">
+          <MenuList />
+        </div>
+        <div class="w3-col m4">
+          <MenuList />
+        </div>
+      </div>
+      <MenuList />
+
       <div class="w3-container w3-cell">
         <a
           className="App-link"
@@ -28,7 +32,11 @@ export default function App() {
         </a>
       </div>
       <br />
-      <Order />
+      <div class="w3-container w3-cell">
+        {" "}
+        <CreateOrder />
+      </div>
+
       <br />
     </>
   );
