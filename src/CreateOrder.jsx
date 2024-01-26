72db4f7e094e12e20d781620df3f00f7c6e9b6c4
albert <albertprofe@gmail.com>
2024-01-26 06:36:56 +0100

    #feat create order

--- src/CreateOrder.jsx
+++ src/CreateOrder.jsx
@@ -2,10 +2,23 @@ import "w3-css/w3.css";
 import React, { useState } from "react";
 
 
+
+
 export default function CreateOrder() {
 
- const [orderId, setOrderId] = useState('');
-   const [selectedWaiter, setSelectedWaiter] = useState("");
+  const currentOrderInit = {
+    "orderId": "order#0",
+    "date": "",
+    "waiter": "",
+    "table": "",
+    "qtyPeople": 0,
+    "menusSelected": [0, 0, 0],
+  };
+
+ const [currentOrder, setCurrentOrder] = useState(currentOrderInit);
+
+  const [orderId, setOrderId] = useState('');
+  const [selectedWaiter, setSelectedWaiter] = useState("");
   const [selectedTable, setSelectedTable] = useState('');
   const [numberOfPeople, setNumberOfPeople] = useState('');
   const [menu1Quantity, setMenu1Quantity] = useState('');
@@ -51,6 +64,17 @@ export default function CreateOrder() {
 
     //console.log("Date:", currentDateString);
 
+    setCurrentOrder( {
+      "orderId": newOrderId,
+      "date": currentDateString,
+      "waiter": selectedWaiter,
+      "table": selectedTable,
+      "qtyPeople": numberOfPeople,
+      "menusSelected": [menu1Quantity,
+      menu2Quantity,
+      menu3Quantity]
+    });
+
     // Process the order or send it to a server
     console.log("Order Submitted:", {
       orderId: newOrderId,
@@ -63,7 +87,10 @@ export default function CreateOrder() {
       menu3Quantity,
     });
 
+
+
     // Clear form fields for the next order
+    setOrderId("");
     setSelectedTable("");
     setSelectedWaiter("");
     setNumberOfPeople("");
@@ -77,73 +104,120 @@ export default function CreateOrder() {
     <>
       <h2>Create an Order</h2>
       <div class="w3-container">
-        <div class="w3-panel w3-card-4 w3-sand">
-          <h1>Order</h1>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <s><label>Order ID:</label>
-              <span>{orderId}</span></s>
+        <div class="w3-row">
+          <div class="w3-col m6">
+            <div class="w3-panel w3-card-4 w3-light-grey">
+              <h1>Order</h1>
+              <form onSubmit={handleSubmit}>
+                <div>
+                  <s>
+                    <label>Order ID:</label>
+                    <span>{orderId}</span>
+                  </s>
+                </div>
+                <div>
+                  <label>Select Waiter:</label>
+                  <select value={selectedWaiter} onChange={handleWaiterChange}>
+                    <option value="">Select a waiter</option>
+                    <option value="waiter1">George</option>
+                    <option value="waiter2">Paul</option>
+                    <option value="waiter3">Ringo</option>
+                    <option value="waiter4">John</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Select Table:</label>
+                  <select value={selectedTable} onChange={handleTableChange}>
+                    <option value="">Select a Table</option>
+                    <option value="table1">Table 1</option>
+                    <option value="table2">Table 2</option>
+                    <option value="table3">Table 3</option>
+                  </select>
+                </div>
+                <div>
+                  <label>Number of People:</label>
+                  <input
+                    type="number"
+                    value={numberOfPeople}
+                    onChange={handleNumberOfPeopleChange}
+                  />
+                </div>
+                <div>
+                  <label>Menu 1 Quantity:</label>
+                  <input
+                    type="number"
+                    value={menu1Quantity}
+                    onChange={handleMenu1QuantityChange}
+                  />
+                </div>
+                <div>
+                  <label>Menu 2 Quantity:</label>
+                  <input
+                    type="number"
+                    value={menu2Quantity}
+                    onChange={handleMenu2QuantityChange}
+                  />
+                </div>
+                <div>
+                  <label>Menu 3 Quantity:</label>
+                  <input
+                    type="number"
+                    value={menu3Quantity}
+                    onChange={handleMenu3QuantityChange}
+                  />
+                </div>
+                <div>
+                  <s>
+                    <label>Order Date:</label>
+                    <span>{orderDate}</span>
+                  </s>
+                </div>
+                <br />
+                <button class="w3-button w3-pink" type="submit">
+                  Submit Order
+                </button>
+              </form>
+              <br />
             </div>
-            <div>
-              <label>Select Waiter:</label>
-              <select value={selectedWaiter} onChange={handleWaiterChange}>
-                <option value="">Select a waiter</option>
-                <option value="waiter1">George</option>
-                <option value="waiter2">Paul</option>
-                <option value="waiter3">Ringo</option>
-                <option value="waiter4">John</option>
-              </select>
+          </div>
+
+          <div class="w3-col m6">
+            <div class="w3-container">
+              {/* Display individual properties of currentOrder */}
+              <div class="w3-panel w3-display-container">
+                <span
+                  onclick="this.parentElement.style.display='none'"
+                  class="w3-button w3-large w3-display-topright"
+                >
+                  &times;
+                </span>
+
+                <div>
+                  <h3>Current submitted order</h3>
+                </div>
+
+                <div>
+                  <strong>Order ID:</strong> {currentOrder.orderId}
+                </div>
+                <div>
+                  <strong>Date:</strong> {currentOrder.date}
+                </div>
+                <div>
+                  <strong>Waiter:</strong> {currentOrder.waiter}
+                </div>
+                <div>
+                  <strong>Table:</strong> {currentOrder.table}
+                </div>
+                <div>
+                  <strong>Number of People:</strong> {currentOrder.qtyPeople}
+                </div>
+                <div>
+                  <strong>Menus Selected:</strong>{" "}
+                  {currentOrder.menusSelected.join(", ")}
+                </div><br/>
+              </div>
             </div>
-            <div>
-              <label>Select Table:</label>
-              <select value={selectedTable} onChange={handleTableChange}>
-                <option value="">Select a Table</option>
-                <option value="table1">Table 1</option>
-                <option value="table2">Table 2</option>
-                <option value="table3">Table 3</option>
-              </select>
-            </div>
-            <div>
-              <label>Number of People:</label>
-              <input
-                type="number"
-                value={numberOfPeople}
-                onChange={handleNumberOfPeopleChange}
-              />
-            </div>
-            <div>
-              <label>Menu 1 Quantity:</label>
-              <input
-                type="number"
-                value={menu1Quantity}
-                onChange={handleMenu1QuantityChange}
-              />
-            </div>
-            <div>
-              <label>Menu 2 Quantity:</label>
-              <input
-                type="number"
-                value={menu2Quantity}
-                onChange={handleMenu2QuantityChange}
-              />
-            </div>
-            <div>
-              <label>Menu 3 Quantity:</label>
-              <input
-                type="number"
-                value={menu3Quantity}
-                onChange={handleMenu3QuantityChange}
-              />
-            </div>
-            <div>
-              <s><label>Order Date:</label>
-              <span>{orderDate}</span></s>
-            </div>
-            <br />
-            <button class="w3-button w3-pink" type="submit">
-              Submit Order
-            </button>
-          </form>
+          </div>
         </div>
       </div>
     </>
