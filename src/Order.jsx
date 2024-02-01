45173d7ee10a5755bb56cac2b5e3d4152f5a5fb7
albert <albertprofe@gmail.com>
2024-02-01 16:56:53 +0100

    useState example

--- src/Order.jsx
+++ src/Order.jsx
@@ -1,10 +1,19 @@
 import "w3-css/w3.css";
+import React, { useState } from "react";
 
-export default function Order () {
+export default function Order() {
+  const [count, setCount] = useState(0);
+  console.log(count);
+
   return (
     <>
       <div class="w3-container">
-        <button class="w3-button w3-pink">create new order</button>
+        <h1>My first hook: useState</h1>
+
+        <p>You clicked {count} times</p>
+        <button class="w3-button w3-blue" onClick={() => setCount(count + 1)}>
+          Click me
+        </button>
       </div>
     </>
   );
